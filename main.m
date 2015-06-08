dc26c2fc70f20937259579a34f0263b7bf8ee30f
Michal Rus <m@michalrus.com>
2015-06-08 15:57:37 +0200

    Read the config file

--- main.m
+++ main.m
@@ -1,3 +1,6 @@
 (* ::Package:: *)
 
-
+(* read config *)
+configRaw=ReadString["./config"];
+ToExpression[StringReplace[StringCases[configRaw, RegularExpression["(^|\\s)\\S+\\s*=\\s*[\\d.]+"]], {"_"->""}]];
+ToExpression[StringReplace[StringCases[configRaw, RegularExpression["(^|\\s)\\S+\\s*=\\s*'.*?'"]],   {"_"->"", "'"->"\""}]];
