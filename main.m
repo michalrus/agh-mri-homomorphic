56f9a213e8f184e045b8f2ef8f167eb7e7876afd
Michal Rus <m@michalrus.com>
2015-06-09 21:03:07 +0200

    Read input files

--- main.m
+++ main.m
@@ -1,6 +1,28 @@
 (* ::Package:: *)
 
+BeginPackage["MRI`"]
+
+
 (* read config *)
-configRaw=ReadString["./config"];
-ToExpression[StringReplace[StringCases[configRaw, RegularExpression["(^|\\s)\\S+\\s*=\\s*[\\d.]+"]], {"_"->""}]];
-ToExpression[StringReplace[StringCases[configRaw, RegularExpression["(^|\\s)\\S+\\s*=\\s*'.*?'"]],   {"_"->"", "'"->"\""}]];
+ToExpression[Map[
+  Function[kv,
+    StringReplace[StringTrim[Part[kv,1]],"_"->""]
+    <>"="<>
+    StringReplace[StringTrim[Part[kv,2]],"'"->"\""]],
+    StringSplit[StringCases[ReadString["./config"], RegularExpression["(^|\\s)\\S+\\s*=\\s*([\\d.]+|'.*?')"]],"="]]];
+
+
+(* I/O functions *)
+normalizePixel[max_][pix_]:=N[If[Length[pix]<3,First[pix],Total[Take[pix,3]]/3]/max];
+
+readInputFile[path_]:=If[StringMatchQ[path,"*.csv",IgnoreCase->True],
+  Import[path,"CSV"],
+  Map[Function[row,Map[normalizePixel[255],row]],Import[path,"Data"]]];
+
+
+(* read the input files *)
+noisyMrImage:=readInputFile[inputfilename];
+snr:=readInputFile[inputfilenamesnr];
+
+
+EndPackage[]
